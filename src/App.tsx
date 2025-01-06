c691c57eb894e57565cbd273d58cd9b04c40c9c4
ZorX032 <shuriknich032@gmail.com>
2025-01-06 15:37:14 +0200

    commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-
+import './App.css'
+import {products} from "./data/productsList.ts";
+import MyProduct from "./components/my-product/MyProduct.tsx";
+// import MyComponent from "./components/my-component/MyComponent.tsx";
 
 function App() {
 
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            {
+                products.map((product, index) => <MyProduct key={index} product={product}/>
+                )
+            }
+        </>
+    );
 }
 
 export default App
