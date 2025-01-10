51010f32b2ae1a07d8fbcc987aa68b75b681a61d
ZorX032 <shuriknich032@gmail.com>
2025-01-10 13:10:16 +0200

    lesson_2_2_end

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -4,11 +4,12 @@
 
 import {ITodo} from "../models/ITodo.ts";
 const baseUrl = import.meta.env.VITE_API_URL;
+
+
 export const getTodos = async ():Promise<ITodo[]> => {
-    const todos =  await fetch(baseUrl)
-        .then(value => value.json())
-    console.log('Fetched todos:', todos);
-    return todos;
+    const response =  await fetch(baseUrl).then(value => value.json())
+    
+    return response.todos;
 }
 
 // export const getTodo = async (id:string): Promise<ITodo> => {
