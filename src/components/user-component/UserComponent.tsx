b8c936d0369b91097016ed625ef93cccafc098e9
ZorX032 <shuriknich032@gmail.com>
2025-01-10 18:04:15 +0200

    hw_3.2_start

--- src/components/user-component/UserComponent.tsx
+++ src/components/user-component/UserComponent.tsx
@@ -1,15 +1,20 @@
 import {FC} from "react";
 import {IUser} from "../../models/IUser.ts";
-import {Link} from "react-router-dom";
-// import {Link, useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 type UserTypeProps={
     item: IUser;
 }
 const UserComponent: FC<UserTypeProps> = ({item}) => {
+    const navigate = useNavigate();
+    const handleOnClick = () => {
+        navigate('details', {state: item});
+    }
     return (
         <div>
             <Link to={'details'} state={item}>{item.username}</Link>
+
+            <button onClick={handleOnClick}>go to details</button>
 
         </div>
     );
