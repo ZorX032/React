02b48aa16b8958ad666f78379897808f4f4ac096
ZorX032 <shuriknich032@gmail.com>
2025-01-15 13:58:39 +0200

    react_lesson_5.1 - 5.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import './App.css'
-import {products} from "./data/productsList.ts";
-import MyProduct from "./components/my-product/MyProduct.tsx";
-// import MyComponent from "./components/my-component/MyComponent.tsx";
+import FormComponent from "./components/FormComponent.tsx";
+import {FC} from "react";
 
-function App() {
+
+const App:FC = () => {
 
 
     return (
         <>
-            {
-                products.map((product, index) => <MyProduct key={index} product={product}/>
-                )
-            }
+            <FormComponent/>
         </>
+
+
     );
 }
 
---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,58 @@
+import {FormEvent, useState} from "react";
+
+
+interface IFormProps {
+    username: string,
+    password: string;
+}
+
+const FormComponent = () => {
+
+    const [formState, setFormState] = useState<IFormProps>({
+        username: 'foobar',
+        password: '1111'
+    })
+
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = {
+            username: formState.username,
+            password: formState.password
+        }
+        console.log(user);
+    };
+
+    //Перший спосіб
+    // const handleUsernameChange = (e: FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLInputElement;
+    //     console.log(input.value);
+    //     setFormState({...formState, username: input.value});
+    // }
+    //
+    // const handlePasswordChange = (e: FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLInputElement;
+    //     console.log(input.value);
+    //     setFormState({...formState, password: input.value});
+    // }
+
+
+    //Другий спосіб
+    const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+        const input = e.target as HTMLInputElement;
+        console.log(input.name);
+        setFormState({...formState, [input.name]: input.value});
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
+                <input type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+                <button>send</button>
+            </form>
+        </div>
+    );
+};
+
+export default FormComponent;
