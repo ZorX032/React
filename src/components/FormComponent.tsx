51651f3fc301fe7f7eea1563bd125aa75ec17e1f
ZorX032 <shuriknich032@gmail.com>
2025-01-15 14:53:45 +0200

    react_lesson_5.4 end

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -10,7 +10,10 @@ interface IFormProps {
 
 const FormComponent = () => {
 
-    const {handleSubmit, register} = useForm<IFormProps>();
+    const {handleSubmit, register, formState:{errors, isValid}}
+        = useForm<IFormProps>({
+        mode:'all'
+    });
 
 
     const customHandler = (formDataProps:IFormProps) => {
@@ -21,10 +24,29 @@ const FormComponent = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(customHandler)}>
-                <input type="text" {...register('username')}/>
-                <input type="text" {...register('password')}/>
-                <input type="number" {...register('age')}/>
-                <button>send</button>
+                <label><input type="text" {...register('username', {
+                    required: {value:true, message:'name is required'},
+                    minLength: {value: 4, message: 'wrong name'},
+
+                })}/>
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+                <label><input type="text" {...register('password', {
+                    required: true,
+                    minLength: {value: 3, message: 'pass too short'},
+                    maxLength: {value: 6, message: 'pass too long'}
+                })}/>
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+                <label><input type="number" {...register('age', {
+                    required: true,
+                    valueAsNumber: true,
+                    min: {value: 1, message: 'age too small'},
+                    max: {value: 117, message: 'age too big'}
+                })}/>
+                    {errors.age && <div>{errors.age.message}</div>}
+                </label>
+                <button disabled={!isValid}>send</button>
             </form>
         </div>
     );
