e22a080008ab92995186de76c17b25e1a2819356
ZorX032 <shuriknich032@gmail.com>
2025-01-15 16:14:29 +0200

    react_lesson_5.6 end axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
-import FormComponent from "./components/FormComponent.tsx";
-import {FC} from "react";
+
+import {FC, useEffect} from "react";
+import {getAllUsers, saveUser} from "./services/user.service.ts";
 
 
 const App:FC = () => {
-
+    useEffect(()=>{
+        getAllUsers().then(value => console.log(value));
+        saveUser({id: 1, name: "John", email:"john@gmail.com"}).then(value => console.log(value));
+    },[]);
 
     return (
         <>
-            <FormComponent/>
+
         </>
 
 
