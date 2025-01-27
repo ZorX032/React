481ab48f250988d03c1c16de322ebca5dcce5a3c
ZorX032 <shuriknich032@gmail.com>
2025-01-27 15:31:36 +0200

    react_lesson_8.1 redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,43 @@
-
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {routes} from "./router/routes.tsx";
 import {RouterProvider} from "react-router-dom";
+import {Provider, useSelector} from "react-redux";
+import {configureStore, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {IUser} from "./models/IUser.ts";
 
+type UserSliceType = {
+    users: IUser[];
+}
+const initialState: UserSliceType = {users: []};
 
+const userSlice = createSlice({
+    name: "userSlice",
+    initialState: initialState,
+    reducers: {
+        loadUsers:  (state, action: PayloadAction<IUser[]>) => {
+            state.users = action.payload;
+        }
+    }
 
+})
 
+export const userSliceActions ={
+    ...userSlice.actions
+}
 
-createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>);
+const store = configureStore({
+    reducer: {
+        userSlice: userSlice.reducer,
+        //postSlice:null
+    }
+})
+
+export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>();
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <RouterProvider router={routes}/>
+    </Provider>
+)
+
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,10 +1,31 @@
-import UsersComponent from "../components/users/UsersComponent.tsx";
+
+import {useAppSelector, userSliceActions} from "../main.tsx";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
 
 
-const UsersPage = () => {
+
+
+
+ export const UsersPage = () => {
+    const {users} = useAppSelector(({userSlice}) => userSlice);
+     const dispatch = useDispatch();
+
+     useEffect(()=>{
+         fetch('https://jsonplaceholder.typicode.com/users')
+             .then(value => value.json())
+             .then(value=>{
+                 dispatch(userSliceActions.loadUsers(value))
+             })
+     })
+
     return (
         <div>
-            <UsersComponent/>
+            {
+                users.map( (user) => {
+                    return <div key={user.id}>{user.username}</div>
+                })
+            }
         </div>
     );
 };
